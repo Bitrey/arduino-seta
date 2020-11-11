4b0b957d72a53214076474026c1e40bfd6c249f8
Bitrey <info@bitrey.it>
2020-11-11 23:05:08 +0100

    working i think

--- seta.ino
+++ seta.ino
@@ -2,16 +2,12 @@
 #include <Ethernet.h>
 #include <SPI.h>
 
-//#define CODICE_FERMATA "MO2076"
-//#define FERMATA_OPPOSTA "si"
+#define CODICE_FERMATA "MO506"
+#define FERMATA_OPPOSTA "si"
 
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-// set the LCD address to 0x27 for a 16 chars and 2 line display
-
-// const char* CODICE_FERMATA = "MO2076";
-// const char* corseFermataOpposta = "si"
 
 EthernetClient client;
 
@@ -34,7 +30,7 @@ void initializeLCD() {
 void initializeSerial() {
     Serial.begin(9600);
     while (!Serial) continue;
-    Serial.println(F("Comunicazione seriale stabilita"));
+    Serial.println("Comunicazione seriale stabilita");
 }
 
 // Initialize internet connection via ethernet
@@ -69,18 +65,6 @@ void checkHttpStatus() {
     }
 }
 
-// Make request
-void makeRequest(char *url) {
-    //  USE URL!!!
-    client.println(F("GET /codice/MO2076 HTTP/1.0"));
-    client.println(F("Host: setaapi2.bitrey.it"));
-    client.println(F("Connection: close"));
-    if (client.println() == 0) {
-        Serial.println(F("Failed to send request"));
-        return;
-    }
-}
-
 // Skip HTTP headers
 void skipHttpHeaders() {
     char endOfHeaders[] = "\r\n\r\n";
@@ -97,20 +81,23 @@ int cursoreFermata() {
 void setup() {
     initializeLCD();
 
-    initializeSerial();
+    // DEBUG
+    // initializeSerial();
 
     initializeEthernet();
 
-    // Connect to HTTP server
     connectToServer();
 
-    // Make request
-    makeRequest("/codice/MO2076");
+    client.println(F("GET /codice/MO506 HTTP/1.0"));
+    client.println(F("Host: setaapi2.bitrey.it"));
+    client.println(F("Connection: close"));
+    if (client.println() == 0) {
+        Serial.println(F("Failed to send request"));
+        return;
+    }
 
-    // Check HTTP status
     checkHttpStatus();
 
-    // Skip HTTP headers
     skipHttpHeaders();
 
     // Allocate the JSON document
@@ -134,10 +121,10 @@ void setup() {
     strcpy(oraAttuale, doc["orario"]);
     //  nomeFermata = doc["nome"];
     //  Calcola centro
-    Serial.println(F("Nome fermata:"));
-    Serial.println(nomeFermata);
-    Serial.println(F("Orario:"));
-    Serial.println(oraAttuale);
+    // Serial.println(F("Nome fermata:"));
+    // Serial.println(nomeFermata);
+    // Serial.println(F("Orario:"));
+    // Serial.println(oraAttuale);
     lcd.setCursor(cursoreFermata(), 0);
     lcd.print(nomeFermata);
     lcd.setCursor(5, 1);
@@ -150,53 +137,19 @@ void setup() {
 }
 
 void loop() {
-    // DEBUG
-    Serial.println(F("mo mi connetto al server"));
-    // Connect to HTTP server
     connectToServer();
-    // DEBUG
-    Serial.println(F("OK! ora aspettiamo POI parte richiesta"));
-    delay(1000);
 
-    // DEBUG
-    // Serial.println(F("ok, ora richiesto"));
-
-    // Make request
-    //  makeRequest("/codice/MO2076");
-    // Do this until URL is fixed
-    // DEBUG
-    Serial.println(F("ora FACCIAMO RICHIESTA"));
-    client.println(F("GET /corse/mo/MO2076?corseFermataOpposta=si HTTP/1.0"));
+    client.println(F("GET /corse/mo/MO506?corseFermataOpposta=si HTTP/1.0"));
     client.println(F("Host: setaapi2.bitrey.it"));
     client.println(F("Connection: close"));
     if (client.println() == 0) {
         Serial.println(F("Failed to send request"));
         return;
     }
-    // DEBUG
-    Serial.println(F("FATTO!! wait poi check http status"));
-    delay(1000);
 
-    // DEBUG
-    // Serial.println(F("checking http status!"));
-
-    // Check HTTP status
     checkHttpStatus();
-    // DEBUG
-    Serial.println(F("CONTROLLATO, ora wait poi headers"));
-    delay(1000);
 
-    // DEBUG
-    // Serial.println(F("skipping http headers!"));
-
-    // Skip HTTP headers
     skipHttpHeaders();
-    // DEBUG
-    Serial.println(F("fatto, poi allocazione"));
-    delay(1000);
-
-    // DEBUG
-    // Serial.println(F("clearing lcd!"));
 
     // Allocate the JSON document
     // Use arduinojson.org/v6/assistant to compute the capacity.
@@ -212,14 +165,8 @@ void loop() {
         return;
     }
 
-    // DEBUG
-    // Serial.println(F("allocated, now showing"));
-
     // Viene impostato a vero se qualche autobus viene stampato
     bool noBuses = true;
-    // DEBUG
-    Serial.println(F("allocato, ora loopo e clearo lcd"));
-    delay(1000);
 
     lcd.clear();
 
@@ -234,21 +181,19 @@ void loop() {
         const int PROGMEM minArrivo = bus["min_all_arrivo_val"];
         const char PROGMEM *tempoReale = bus["temporeale"];
 
-        if (!linea || strlen(linea) == 0) { /* continue ; */
-            //      DEBUG
-            Serial.println("No linea");
-        }
+        // if (!linea || strlen(linea) == 0) { /* continue ; */
+        //     //      DEBUG
+        //     Serial.println(F("No linea"));
+        // }
 
         noBuses = false;
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print(linea);
-        //    Serial.println(strlen(destinazione));
+
         lcd.setCursor(strlen(linea) + 1, 0);
-        //    const char destSubstr[16];
-        //    strncpy(destSubstr, destinazione, strlen(linea) + 15 -
-        //    strlen(destinazione));
         lcd.print(destinazione);
+
         if (minArrivo != 9999) {
             lcd.setCursor(0, 1);
             lcd.print(tempoReale);
@@ -259,26 +204,6 @@ void loop() {
             lcd.setCursor(0, 1);
             lcd.print(pianificato);
             lcd.print(" (pianific)");
-        }
-        //    lcd.setCursor(6, 1);
-        //    lcd.print("Real");
-        //    lcd.setCursor(11, 1);
-        //    lcd.print(minArrivo != 9999 ? tempoReale : "N/A");
-
-        Serial.println(F("*************"));
-        Serial.println(F("Linea:"));
-        Serial.println(linea);
-        //    Serial.println(sizeof(linea));
-        Serial.println(F("Destinazione:"));
-        Serial.println(destinazione);
-        Serial.println(F("Pianificato:"));
-        Serial.println(pianificato);
-        if (minArrivo != 9999) {
-            Serial.println(F("Tempo reale:"));
-            Serial.println(tempoReale);
-        } else {
-            Serial.println(F("Tempo reale:"));
-            Serial.println("sconosciuto");
         }
 
         delay(3000);
@@ -295,13 +220,7 @@ void loop() {
         }
         lcd.setCursor(0, 1);
         lcd.print("Nessuna corsa");
-        // DEBUG
-        Serial.println(F("Nessuna corsa"));
     }
-
-    // DEBUG
-    Serial.println(F("Dimensione array"));
-    Serial.println(arr.size());
 
     delay(1000);
 
